362bf5cc67eea4f59734cbfbab5a85b9bc83e5bc
Akshat Tiwari <itsakshatts@gmail.com>
2025-08-12 00:12:35 +0530

    feat: implement Kafka producer in metrics agent

--- services/metrics-collector/go.mod
+++ services/metrics-collector/go.mod
@@ -6,13 +6,16 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/prometheus/client_golang v1.23.0 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.65.0 // indirect
 	github.com/prometheus/procfs v0.16.1 // indirect
+	github.com/segmentio/kafka-go v0.4.48 // indirect
 	github.com/shirou/gopsutil/v3 v3.24.5 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
--- services/metrics-collector/main.go
+++ services/metrics-collector/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/segmentio/kafka-go"
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/mem"
 )
@@ -23,21 +27,54 @@ var (
 	})
 )
 
-func recordMetrics() {
+type MetricMessage struct {
+	Hostname  string    `json:"hostname"`
+	Timestamp time.Time `json:"timestamp"`
+	CPU_Usage float64   `json:"cpu_usage_percent"`
+	MEM_Usage float64   `json:"mem_usage_percent"`
+}
+
+func recordMetrics(kafkaWriter *kafka.Writer) {
+	hostname, _ := os.Hostname()
+
 	go func() {
 		for {
 			cpuPercentages, err := cpu.Percent(time.Second, false)
 			if err != nil {
 				log.Printf("Error getting CPU usage: %v", err)
-			} else if len(cpuPercentages) > 0 {
-				cpuUsage.Set(cpuPercentages[0])
+				continue
 			}
+			cpuVal := cpuPercentages[0]
 
 			vMem, err := mem.VirtualMemory()
 			if err != nil {
 				log.Printf("Error getting memory usage: %v", err)
+				continue
+			}
+			memVal := vMem.UsedPercent
+			cpuUsage.Set(cpuVal)
+			memUsage.Set(memVal)
+
+			msg := MetricMessage{
+				Hostname:  hostname,
+				Timestamp: time.Now(),
+				CPU_Usage: cpuVal,
+				MEM_Usage: memVal,
+			}
+
+			jsonMsg, err := json.Marshal(msg)
+			if err != nil {
+				log.Printf("Error marshalling JSON: %v", err)
+				continue
+			}
+
+			err = kafkaWriter.WriteMessages(context.Background(), kafka.Message{
+				Value: jsonMsg,
+			})
+			if err != nil {
+				log.Printf("Could not write message to kafka: %v", err)
 			} else {
-				memUsage.Set(vMem.UsedPercent)
+				log.Printf("Wrote message to Kafka: %s", string(jsonMsg))
 			}
 
 			time.Sleep(2 * time.Second)
@@ -46,10 +83,16 @@ func recordMetrics() {
 }
 
 func main() {
-	recordMetrics()
+	kafkaBroker := "kafka:29092"
+	topic := "metrics-raw"
+	writer := &kafka.Writer{
+		Addr:     kafka.TCP(kafkaBroker),
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	}
 
+	recordMetrics(writer)
 	http.Handle("/metrics", promhttp.Handler())
 	log.Println("Beginning to serve on port :9100")
-
 	log.Fatal(http.ListenAndServe(":9100", nil))
 }
